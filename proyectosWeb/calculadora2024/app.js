c4eb1a7252956fb9c5fbf5a4746395bfccf49fa8
sebastian carrero <mugssebastian@gmail.com>
2024-01-17 23:56:06 -0500

    fin calculadora

--- proyectosWeb/calculadora2024/app.js
+++ proyectosWeb/calculadora2024/app.js
@@ -11,12 +11,50 @@ let numeroReinicia = false;
 //eventos
 botonesNumeros.forEach(boton => boton.addEventListener("click",(e) =>numeroClickeado(e.target.textContent)));
 botonesOperaciones.forEach(boton => boton.addEventListener("click",(e) =>operacionClickeada(e.target.textContent)));
+document.getElementById("punto").addEventListener("click",punto);
+document.getElementById("clear").addEventListener("click",clear);
+document.getElementById("allClear").addEventListener("click",allClear);
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            numeroClickeado(parseInt(e.key));
+        break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            operacionClickeada(e.key);
+        break;
+        case "Enter":
+            operacionClickeada("=");
+        break;
+        case "Escape":
+            allClear()
+        break;
+        case "Backscape":
+            clear()
+        break;
+        case ".":
+            punto()
+        break;
+    }
+});
 
 //funciones
 function numeroClickeado(numero){
     //console.log(numero)
     if(numeroReinicia){
         pantallaElement.textContent = parseFloat(numero);
+        numeroReinicia = false;
     }else{
         pantallaElement.textContent = parseFloat(pantallaElement.textContent + numero);
     }
@@ -55,3 +93,18 @@ function operacionClickeada(operacion){
     console.log(operacion , numeroAnterior);
 }
 
+function punto(){
+    if(Number.isInteger(parseFloat(pantallaElement.textContent))){
+        pantallaElement.textContent += "."
+    }
+}
+
+function clear(){
+    pantallaElement.textContent = 0;
+}
+
+function allClear(){
+    clear();
+    numeroAnterior = undefined;
+    indicadorOperacion.textContent = undefined;
+}
