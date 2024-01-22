4388c20aecece84825d867ce5f915eee8e226c5e
sebastian carrero <mugssebastian@gmail.com>
2024-01-21 19:25:59 -0500

    juego de respuestas 70%

--- proyectosWeb/RespondeJuego/app.js
+++ proyectosWeb/RespondeJuego/app.js
@@ -1,6 +1,13 @@
 //Referencias
 const icono = document.getElementById("icono");
 const tooltip = document.getElementById("tooltip");
+const fraseElement = document.getElementById("frase");
+
+//variables
+let fraseSecreta = "Por favor me gustaria que me respondas";
+let respuestaSecreta = "";
+let estadoTruco = false;
+
 
 //tooltip
 icono.addEventListener("mouseenter", () =>{
@@ -11,3 +18,23 @@ icono.addEventListener("mouseenter", () =>{
 icono.addEventListener("mouseleave", () =>{
     tooltip.classList.toggle("escondido",true)
 });
+
+fraseElement.addEventListener("input",(e)=>
+    {//console.log(e.data)
+    if(e.data === "-"){
+        estadoTruco = !estadoTruco
+        escribirLetraCorrecta()
+    };
+    if(estadoTruco){
+        respuestaSecreta += e.data;
+        escribirLetraCorrecta();
+        console.log(respuestaSecreta)
+    }
+    }
+)
+
+function escribirLetraCorrecta(){
+    const letraAAgregar = fraseSecreta[fraseElement.value.length-1];
+    if(letraAAgregar === undefined) return estadoTruco=false;
+    fraseElement.value = fraseElement.value.substring(0,fraseElement.value.length-1 )+letraAAgregar;
+}
