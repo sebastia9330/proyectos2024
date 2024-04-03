f2b5ca96967902f21b010e90e7ba37159e5a7e1a
sebastian carrero <mugssebastian@gmail.com>
2024-04-03 02:31:12 -0500

    cuarta parte

--- proyectosWeb/juegopong/app.js
+++ proyectosWeb/juegopong/app.js
@@ -7,6 +7,7 @@ class Paleta{
     velocidad = 5;
     movimiento;
     alto = 200;
+    ancho = 20
 
     constructor(){
         this.element = document.createElement("div");
@@ -48,15 +49,76 @@ class Paleta{
 }
 
 class Bola{
+    x;
+    y;
+    dx = -10;
+    dy = -10;
+    movimiento;
+    ancho = 30;
+
     constructor(){
         this.element = document.createElement("div");
         this.element.classList = "bola";
         zonaJuego.appendChild(this.element);
+        this.resetPosicion();
+    }
+
+    resetPosicion(){
+        this.x = (document.body.clientWidth/2) - this.ancho;
+        this.element.style.left = this.x + "px";
+        this.y = (document.body.clientHeight/2) - this.ancho;
+        this.element.style.top = this.y + "px";
+    }
+
+    mover(){
+        if(!this.movimiento){
+            this.movimiento = setInterval(()=>{
+                //Movimiento Horizontal
+                this.x += this.dx;
+
+                //choque con paletas
+                //paleta J1
+                if(this.x < 0 + J1.ancho &&
+                    this.y + this.ancho/2 > J1.y &&
+                    this.y + this.ancho/2 < J1.y + J1.alto){
+                        this.dx = this.dx* -1
+                }
+
+                //Paleta J2
+                if(this.x + this.ancho > document.body.clientWidth - J2.ancho &&
+                    this.y + this.ancho/2 > J2.y &&
+                    this.y + this.ancho/2 < J2.y + J2.alto){
+                    this.dx = this.dx* -1
+                }
+
+                //meter Punto
+                if(this.x < 0 || this.x > document.body.clientWidth - this.ancho){
+                    console.log("punto") 
+                    this.eliminar();
+                }
+
+
+                if(this.x < 0 || this.x > document.body.clientWidth - this.ancho){
+                    this.dx = this.dx *-1
+                }
+                this.element.style.left = this.x + "px";
+                //Movimiento Vertical
+                this.y += this.dy;
+                if(this.y < 0 || this.y > document.body.clientHeight - this.ancho){
+                    this.dy = this.dy *-1
+                }
+                this.element.style.top = this.y + "px";
+            }, 20)
+        }
+    }
+
+    eliminar(){
+        clearInterval(this.movimiento);
+        zonaJuego.removeChild(this.element)
     }
 }
 
 document.addEventListener("keydown",(e)=>{
-    console.log(e)
     switch(e.key){
         case "w":
             J1.subir();
@@ -75,7 +137,6 @@ document.addEventListener("keydown",(e)=>{
 
 
 document.addEventListener("keyup",(e)=>{
-    console.log(e)
     switch(e.key){
         case "w":
         case "s":
