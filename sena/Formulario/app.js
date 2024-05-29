1409452b55316faf9cdf56f61402a676110b29b5
Sebastian Carrero <mugssebastian@gmail.com>
2024-05-29 16:21:39 -0500

    parte 2

--- sena/Formulario/app.js
+++ sena/Formulario/app.js
@@ -1,43 +1,27 @@
-const express = require('espress');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+document.getElementById('registro1').addEventListener('submit', async (e) =>{
+    e.preventDefault();
+    const formDatos = new FormData(e.target);
+    const data = {
+        nombre: formDatos.get('nombre'),
+        genero: formDatos.get('genero'),
+        autor: formDatos.get('autor')
+    };
 
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/biblioteca', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.log("No hay coneccion"));
-db.once('open', () => {
-    console.log("conoectado a la base de datos");
-});
-
-const libros = new mongoose.Schema({
-    nombre: String,
-    genero: String,
-    autor: String,
-});
-
-const libro = mongoose.model('libro', libros);
-
-app.post('/register', async (req, res) => {
-    const {nombre, genero, autor} = req.body;
-    const nuevoLibro = new libro({nombre, genero, autor});
     try{
-        await nuevoLibro.save();
-        res.status(201).send('Libro registrado con éxito');
-    }catch(error){
-        res.status(400).send('Error al registrar el libro')
-    }
-});
+        const respuesta = await fetch('http://localhost:3000/register',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
 
-app.listen(port, () => {
-    console.log('servidor corriendo en http://localhost:${port}');
+        if(respuesta.ok){
+            alert("Libro registrado con éxito")
+        }else{
+            alert("Error de registro")
+        }
+    }catch(error){
+        alert("Error en la conexión")
+    }
 });
---
+++ sena/backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/biblioteca', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.log("No hay coneccion"));
+db.once('open', () => {
+    console.log("conoectado a la base de datos");
+});
+
+const libros = new mongoose.Schema({
+    nombre: String,
+    genero: String,
+    autor: String,
+});
+
+const libro = mongoose.model('libro', libros);
+
+app.post('/register', async (req, res) => {
+    const {nombre, genero, autor} = req.body;
+    const nuevoLibro = new libro({nombre, genero, autor});
+    try{
+        await nuevoLibro.save();
+        res.status(201).send('Libro registrado con éxito');
+    }catch(error){
+        res.status(400).send('Error al registrar el libro')
+    }
+});
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en http://localhost:${port}`);
+});
