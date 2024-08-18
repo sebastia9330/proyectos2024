70f453d03996a2600805ada0961beff58bb49d82
sebastian carrero <mugssebastian@gmail.com>
2024-08-17 20:40:47 -0500

    clase4.1

---
+++ python/diccionarios_conjuntos.py
@@ -0,0 +1,15 @@
+autos = {"marca": "ford", "modelo": "2015", "año":"2024" }
+print(autos)
+
+autos["color"] = "Rojo"
+print(autos)
+
+
+viajes = {"italia", "china", "rusia"}
+print (viajes)
+
+viajes2 = {"china", "mexico", "japon", "islandia"}
+print(viajes2)
+
+todos_los_viajes = viajes.union(viajes2)
+print(todos_los_viajes)
