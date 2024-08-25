66285b0b82ad8077599f43013e5b7f92ca067e84
Sebastian Carrero <mugssebastian@gmail.com>
2024-08-25 13:13:43 -0500

    CLASE WEB Y CLASE SQL

--- desarrollo-web/clase1/app.js
+++ desarrollo-web/clase1/app.js
@@ -1,3 +1,15 @@
-const invitacion = document.getElementById("invitacion")
+const invitacion = document.getElementById("invitacion");
 
-invitacion.addEventListener("click",()=> alert("sigueme en github como https://github.com/sebastia9330"))
+invitacion.addEventListener("click",()=> alert("sigueme en github como https://github.com/sebastia9330"));
+
+const toggleButton = document.getElementById("toggleTarjetas");
+const tarjetas = document.querySelector(".tarjetas");
+
+toggleButton.addEventListener("click", () =>{
+    if(tarjetas.style.display === "none"){
+        tarjetas.style.display = "flex";
+    }else{
+        tarjetas.style.display = "none";
+    }
+    console.log("hola")
+});
